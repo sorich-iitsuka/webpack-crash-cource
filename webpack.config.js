584392dc0e015ce0f775acb94ac1fb5f3cdb1e76
飯塚裕之 <hiroyuki.iitsuka@sorich.jp>
2023-05-27 23:50:44 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: outputPath
+  },
+  devServer: {
+    contentBase: outputPath
   }
 }
